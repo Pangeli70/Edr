be17ea793ea1452a815444ee3cf324d20d2a8cde
PAngeli70 <pangeli70@gmail.com>
2025-03-15 12:59:45 +0100

    Sorting dependencies

--- srv/monorepo.ts
+++ srv/monorepo.ts
@@ -11,11 +11,11 @@
 
 /* Local >>
 export * from "../../Uts/mod.ts";
-export * from "../../Tng/mod.ts";
 export * from "../../Spc/mod.ts";
 export * as SpcSpecs from "../../Spc/specs.ts";
 export * from "../../Mng/mod.ts";
 export * as MngSpecs from "../../Mng/specs.ts";
+export * from "../../Tng/mod.ts";
 Local << */
 
 
@@ -23,10 +23,10 @@ Local << */
 //------------------
 // Github >>>
 export * from "https://raw.githubusercontent.com/Pangeli70/Uts/master/mod.ts";
-export * from "https://raw.githubusercontent.com/Pangeli70/Tng/master/mod.ts";
-export * from "https://raw.githubusercontent.com/Pangeli70/Mng/master/mod.ts";
 export * from "https://raw.githubusercontent.com/Pangeli70/Spc/master/mod.ts";
-export * as MngSpecs from "https://raw.githubusercontent.com/Pangeli70/Mng/master/specs.ts";
 export * as SpcSpecs from "https://raw.githubusercontent.com/Pangeli70/Spc/master/specs.ts";
+export * from "https://raw.githubusercontent.com/Pangeli70/Mng/master/mod.ts";
+export * as MngSpecs from "https://raw.githubusercontent.com/Pangeli70/Mng/master/specs.ts";
+export * from "https://raw.githubusercontent.com/Pangeli70/Tng/master/mod.ts";
 // export * from "https://raw.githubusercontent.com/Pangeli70/Edr/master/mod.ts";
 // Github <<<
